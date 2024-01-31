20a0bd14e0e2959ec2e71d0a96f70e1774162a06
Vignesh Mohan <so@yahoo.com>
2024-01-31 12:54:36 +0530

    views and db added

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
 rooms = [
@@ -11,15 +13,17 @@ rooms = [
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context )
 
 
 def room(request, pk):
     print(pk)
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    room = Room.objects.get(id = pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
